56048b4ec784a794d741450f8982c210b4fbfa34
T-Nazar <nazar.tukh@gmail.com>
2022-02-23 00:11:49 +0100

    Initial commit

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorInstitutionsAndEconomies"
+  
+  # Name of problem sets in the package
+  ps = c("Institutions_And_Economic_Performance")
+  
+  list(package=package, ps = ps)
+}
